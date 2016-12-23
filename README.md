fedd93dad5f9684b1116c45b99eacc0a7c172892
Joel Handwell <joelhandwell@gmail.com>
2016-12-23 14:30:10 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# odoo
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'odoo'
+maintainer 'Joel Handwell'
+maintainer_email 'joelhandwell@gmail.com'
+license 'apachev2'
+description 'Installs/Configures odoo'
+long_description 'Installs/Configures odoo'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/odoo/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/odoo' if respond_to?(:source_url)
