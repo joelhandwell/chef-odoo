020af57e8ab2fc27ca9924ca808a940c12e4b732
Joel Handwell <joelhandwell@gmail.com>
2016-12-25 20:09:59 -0500

    Install postgresql

---
+++ test/smoke/database/database_test.rb
@@ -0,0 +1,16 @@
+# # encoding: utf-8
+
+# Inspec test for recipe odoo::database
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+describe command('psql --help') do
+  its('stdout') { should include 'psql is the PostgreSQL interactive terminal' }
+end
+
+describe service('postgresql') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
