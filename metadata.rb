62be7ccd4c9605dcfec66bb4b1ecb10f3f409edc
Joel Handwell <joelhandwell@gmail.com>
2016-12-31 22:16:42 -0500

    Address foodcritic offence FC064, FC065

--- metadata.rb
+++ metadata.rb
@@ -10,13 +10,13 @@ version '0.1.1'
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/odoo/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/gospelandit/chef-odoo/issues' if respond_to?(:issues_url)
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/odoo' if respond_to?(:source_url)
+source_url 'https://github.com/gospelandit/chef-odoo' if respond_to?(:source_url)
 
 depends 'poise-python'
 depends 'ark'
